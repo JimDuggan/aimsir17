05da5decf092f1f1936f02842641ff7c6744452e
Jim Duggan <jim@MyMacBook.local>
2019-11-14 09:41:59 +0000

    Adding stations 1-10

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# aimsir
-Irish Weather Data
+# aimsir17
+Irish Weather Data from Ireland's [Weather Observing Stations](https://www.met.ie/climate/weather-observing-stations) in 2017. Hourly records from January-December based on the following weather observation stations:
+
+- [Athenry, Co. Galway](https://www.met.ie/weather-forecast/athenry-weather-station-galway)
+- [Ballyhaise, Co. Cavan](https://www.met.ie/weather-forecast/ballyhaise-weather-station-cavan)
+- Belmullet, Co. Mayo
+- Carlow Oakpark, Co. Carlow
+
+
+See also, [major weather events](https://www.met.ie/climate/major-weather-events), which in 2017 included [Storm Opelia](https://en.wikipedia.org/wiki/Hurricane_Ophelia_(2017))
+
+
+#### Acknowledgement
+
+- Copyright Met Éireann
+- Source [www.met.ie](http://www.met.ie)
+- This data is published under a [Creative Commons Attribution 4.0 International (CC BY 4.0)]( https://creativecommons.org/licenses/by/4.0/)
+- Met Éireann does not accept any liability whatsoever for any error or omission in the data, their availability, or for any loss or damage arising from their use.
+- This material has been modified from the original, in terms of the following:
--- data-raw/prepare.R
+++ data-raw/prepare.R
@@ -3,8 +3,8 @@ library(lubridate)
 library(dplyr)
 library(devtools)
 
-START_YEAR  <- 2018
-FINISH_YEAR <- 2018
+START_YEAR  <- 2017
+FINISH_YEAR <- 2017
 
 stations <- tibble(
   station = character(),
@@ -24,7 +24,7 @@ prepare <- function(file,station,l_skip=17){
               day=day(date),
               hour=hour(date),
               station=station)    %>%
-       select(station, year, month, day, hour, everything(), -date) %>%
+       select(station, year, month, day, hour, everything()) %>%
        filter(year >= START_YEAR, year <= FINISH_YEAR)
 }
 
@@ -44,19 +44,53 @@ stations   <- add_row(stations,station="BELMULLET",county="Mayo",height=9,latitu
 oak_park <- prepare("data-raw/original data/carlow/hly375/hly375.csv","OAK PARK")
 stations <- add_row(stations,station="OAK PARK",county="Carlow",height=62,latitude=52.861,longitude=-6.915)
 
-# (*) Mace Head weather observing station
+
+# (5) Claremorris
+claremorris <- prepare("data-raw/original data/claremorris/hly2175/hly2175.csv","CLAREMORRIS")
+stations <- add_row(stations,station="CLAREMORRIS",county="Mayo",height=68,latitude=53.711,longitude=-8.993)
+
+# (6) Dunsany
+dunsany <- prepare("data-raw/original data/dunsany/hly1375/hly1375.csv","DUNSANY")
+stations <- add_row(stations,station="DUNSANY",county="Meath",height=83,latitude=53.516,longitude=-6.660)
+
+# (7) Moore Park (Fermoy)
+moore_park <- prepare("data-raw/original data/fermoy/hly575/hly575.csv","MOORE PARK")
+stations <- add_row(stations,station="MOORE PARK",county="Cork",height=46,latitude=52.164,longitude=-8.264)
+
+# (8) Finner
+finner <- prepare("data-raw/original data/finner/hly2075/hly2075 fix2.csv","FINNER",l_skip=0)
+stations <- add_row(stations,station="FINNER",county="Donegal",height=33,latitude=54.494,longitude=-8.243)
+
+# (9) Gurteen
+gurteen <- prepare("data-raw/original data/gurteen/hly1475/hly1475.csv","GURTEEN")
+stations <- add_row(stations,station="GURTEEN",county="Tipperary",height=75,latitude=53.053,longitude=-8.009)
+
+# (10) Johnstown Castle
+j_castle <- prepare("data-raw/original data/johnstown castle/hly1775/hly1775.csv","JOHNSTOWNII")
+stations <- add_row(stations,station="JOHNSTOWNII",county="Wexford",height=62,latitude=52.298,longitude=-6.497)
+
+# (11) Mace Head weather observing station
 mace_head <- prepare("data-raw/original data/mace head/hly275.csv","MACE HEAD")
 stations  <- add_row(stations,station="MACE HEAD",county="Galway",height=21,latitude=53.326,longitude=-9.901)
 
 
-observations <- athenry               %>%
-                bind_rows(ballyhaise) %>%
-                bind_rows(belmullet)  %>%
-                bind_rows(oak_park)   %>%
+observations <- athenry                  %>%
+                bind_rows(ballyhaise)    %>%
+                bind_rows(belmullet)     %>%
+                bind_rows(oak_park)      %>%
+                bind_rows(claremorris)   %>%
+                bind_rows(dunsany)       %>%
+                bind_rows(moore_park)    %>%
+                bind_rows(finner)        %>%
+                bind_rows(gurteen)       %>%
+                bind_rows(j_castle)      %>%
                 bind_rows(mace_head)
 
 use_data(observations,overwrite = T)
 
 # Save Stations tibble
 use_data(stations,overwrite = T)
+
+# Check records
+observations %>% group_by(station) %>% summarise(Records =n(),AvrHourlyRain=mean(rain,na.rm = T))
 
